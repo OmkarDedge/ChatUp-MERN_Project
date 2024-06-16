7ba6553ffed9185085a1bcebc23dd6e4478241b8
OmkarDedge <123588944+OmkarDedge@users.noreply.github.com>
2024-06-15 22:21:10 -0700

    Update chatContext.js

--- frontend/src/context/chatContext.js
+++ frontend/src/context/chatContext.js
@@ -1,4 +1,5 @@
 import { createContext, useContext, useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
 
 const ChatContext = createContext();
 
@@ -8,12 +9,18 @@ const ChatProvider = ({ children }) => {
   const [chats, setChats] = useState([]);
   const [notification, setNotification] = useState([]);
 
+  const history = useHistory();
+
   useEffect(() => {
-    let userInfo = JSON.parse(sessionStorage.getItem("userInfo"));
+    const userInfo = JSON.parse(sessionStorage.getItem("userInfo"));
+
     if (userInfo) {
       setUser(userInfo);
+      history.push("/chats");
+    } else {
+      history.push("/");
     }
-  }, []);
+  }, [history]);
 
   return (
     <ChatContext.Provider
